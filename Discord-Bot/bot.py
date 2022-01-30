a133a8ca01b1b2697932da7a9fdb90b133bc5c9e
Jacob <apone.5@wright.edu>
2022-01-30 14:28:50 -0500

    added error detection

--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -52,5 +52,14 @@ async def on_message(message):
         #response = random.choice(brooklyn_99_quotes)
         response = random.choice(hitchhiker_quotes)
         await message.channel.send(response)
+    
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
 
 client.run(TOKEN)
